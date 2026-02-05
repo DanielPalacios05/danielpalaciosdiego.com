638961179aee36cc5593ca7f9fa051b2cef44e89
Daniel Palacios <drpalaciod@eafit.edu.co>
2026-02-05 17:27:47 -0500

    OGG

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
+import { getMessages, getTranslations } from 'next-intl/server';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import { Geist, Geist_Mono } from "next/font/google";
@@ -20,6 +21,61 @@ const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
 });
+
+export async function generateMetadata({
+    params
+}: {
+    params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+    const { locale } = await params;
+    const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+    return {
+        metadataBase: new URL('https://danielpalaciosdiego.com'),
+        title: {
+            template: '%s | Daniel Palacios',
+            default: t('title')
+        },
+        description: t('description'),
+        openGraph: {
+            title: t('title'),
+            description: t('description'),
+            url: 'https://danielpalaciosdiego.com',
+            siteName: 'Daniel Palacios',
+            images: [
+                {
+                    url: '/images/hero-image.png',
+                    width: 1200,
+                    height: 630,
+                    alt: 'Daniel Palacios'
+                }
+            ],
+            type: 'website',
+            locale: locale
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: t('title'),
+            description: t('description'),
+            images: ['/images/hero-image.png'],
+        },
+        icons: {
+            icon: '/favicon.ico',
+        },
+        robots: {
+            index: true,
+            follow: true,
+            googleBot: {
+                index: true,
+                follow: true,
+                'max-video-preview': -1,
+                'max-image-preview': 'large',
+                'max-snippet': -1,
+            },
+        },
+    };
+}
+
 
 export default async function LocaleLayout({
     children,
